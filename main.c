42c91a094c6c18ec0701480de20cdd4ef04e0448
abhi2399 <32430232+abhi2399@users.noreply.github.com>
2018-04-14 21:00:39 +0530

    Update main.c

--- main.c
+++ main.c
@@ -50,5 +50,106 @@ int main()
     printf("Process p%d,pro[i]);
     printf("with priority %d,pri[i]);
     printf("has burst time %d",bt[i]);
-    printf("\n\n");       
+    printf("\n\n"); 
+  }
+           
+  printf("\n Enter the number of process you want to process in queue 1 :\n");
+  scanf("%d"&x);
+  printf("Enter the number of process you wnat to process in queue 2:\n");
+  scanf("%d",&y);
+  printf("Enter the number of process you want to process in queue 3:\n");
+  scanf("%d",&z);
+    
+           #for Queue 1
+           
+           for(i=0;i<x;i++)
+           {a1[m]=pro[i];
+            m=m+1;
+           }
+           printf("---------------------------------------");
+           printf("| Queue 1    Priority  Burst time  |");
+           printf("--------------------------------------");
+           for(i=0;i<x;i++)
+           {
+           printf("   P%d        ",pro[i]);
+           printf("%d        ",pri[i]);
+           printf("%d",bt[i]);
+           }
+           printf("--------------------------------------");
+           
+            #for Queue 2
+           
+           for(i=x;i<y+x;i++)
+           {a1[m]=pro[i];
+            m=m+1;
+           }
+           printf("---------------------------------------");
+           printf("| Queue 2    Priority  Burst time  |");
+           printf("--------------------------------------");
+           for(i=x;i<y+x;i++)
+           {
+           printf("   P%d        ",pro[i]);
+           printf("%d        ",pri[i]);
+           printf("%d",bt[i]);
+           }
+           printf("--------------------------------------");
+           
+           #for Queue 3
+           
+           for(i=y+x;i<z+y+x;x++)
+           {
+             a1[m]=pro[i];
+             m=m+1;
+           }
+           printf("---------------------------------------");
+           printf("| Queue 1    Priority  Burst time  |");
+           printf("--------------------------------------");
+           for(i=y+x;i<z+y+x;i++)
+           {
+           printf("   P%d        ",pro[i]);
+           printf("%d        ",pri[i]);
+           printf("%d",bt[i]);
+           }
+           printf("--------------------------------------");
+           printf("\n");
+           
+           int tq,t,remain,flag=0;
+           int waiting=0,turn=0;
+           remain=a;
+           printf("\n\n Welcome to QUEUE 1.. LETS DO ROUND ROBIN SCHEDULING...\n\n");
+           for(i=0;i<a;i++)
+           {
+             printf("Enter arrival time for P%d =",pro[i]);
+             scanf("%d",&tq);
+             printf("\n\n Process\t |Turnaround time |Waiting tme \n\n");
+             for(t=0,i=0;remain!=0;)
+             {if(rtr[i]<=tq && rt[i]>0)
+             {t=t+rt[i];
+              rt[i]=0;
+              flag=1;
+             }
+              else if(rt[i]>0)
+              {
+                rt[i]=rt[i]-tq;
+                t=t+tq;
+           }
+              if(rt[i]==0 && flag==1)
+              {remain--;
+               printf("P%d\t|\t%d\t|\t%d\n",i+1,t-at[i],t-at[i]-bt[i]);
+               waiting=waiting+t-at[i]-bt[i];
+               turn=turn+t-at[i];
+               flag=0;
+              }
+              if(i==a-1)
+                i=0;
+              else if(at[i+1]<=t)
+                i++;
+              else
+                i=0;
+             }
+             printf("\\n\nAverage waiting time= %f sec\n",waiting*1.0/a);
+             pritf("\n Average turnaround time=%f sec\n",turn*1.0/a);
+             printf("------------------------------------------------------")
+           
+           
 }
