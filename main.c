572e60f6749ef085ad59868f202112ff56b773e7
abhi2399 <32430232+abhi2399@users.noreply.github.com>
2018-04-14 19:49:03 +0530

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,9 @@ int main()
   scanf("%d",&num);
   int a1[num],a2[num],a3[num];
   int pri[num],pro[num],bt[num],at[num],rt[num];
+  
+  #for taking input
+  
   for(i=0;i<num;i++)
   {
     printf("Enter process name for process %d = ",i+1,"\n");
@@ -18,4 +21,34 @@ int main()
     printf("\n\nEnter he burst time for this process= ");
     scaanf("%d",&bt[i]);
   }  
+  
+  #for sorting the priority
+  for(i=0;i<n-1;i++)
+  {
+    for(j=0;j<(n-i-1);j++)
+    {
+      if(pri[j]>pri[j+1])
+      {
+        temp=pri[j];
+        pri[j]=pri[j+1];
+        pri[j+1]=temp;
+        
+        temp=pro[j];
+        pro[j]=pro[j+1];
+        pro[j+1]=temp;
+        
+        temp=bt[j];
+        bt[j]=bt[j+1];
+        bt[j+1]=temp;
+      }
+    }
+  }
+  #process after the sorting
+  
+  for(i=0;i<num;i++)
+  {
+    printf("Process p%d,pro[i]);
+    printf("with priority %d,pri[i]);
+    printf("has burst time %d",bt[i]);
+    printf("\n\n");       
 }
