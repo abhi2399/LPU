0244b4c6e1a0938b2a1e052c48dd2900480af729
abhi2399 <32430232+abhi2399@users.noreply.github.com>
2018-04-14 19:36:53 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
 #include<stdio.h>
 #include<unistd.h>
 int main()
+{
+  int num,i,j,x,y,z,m=0;
+  int temp=0;
+  printf("Enter number of processes\n");
+  scanf("%d",&num);
+  int a1[num],a2[num],a3[num];
+  int pri[num],pro[num],bt[num],at[num],rt[num];
+  for(i=0;i<num;i++)
+  {
+    printf("Enter process name for process %d = ",i+1,"\n");
+    printf("P");
+    scanf("%d",&pro[i]);
+    printf("\nEnter priority for this process= ");
+    scanf("%d",&pri[i]);
+    printf("\n\nEnter he burst time for this process= ");
+    scaanf("%d",&bt[i]);
+  }  
+}
